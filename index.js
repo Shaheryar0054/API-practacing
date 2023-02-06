dd4481ca1891a72346afa1c205818a6c45c55eb9
shaheryar abid <sharyar0310@gmail.com>
2023-02-06 16:10:19 +0500

    using async and await methode

--- index.js
+++ index.js
@@ -1,2 +1,37 @@
-fetch("README.md").then(respond => respond.text())
-.then(data =>document.write(data));
+const datas = [
+    {name:"shery", Profestion:"software engineer"},
+    {name:"shery", Profestion:"software engineer"}
+];
+
+function getDatas(){
+    setTimeout(() =>{
+        let output = "";
+        datas.forEach((data,index) => {
+            output+=`<li>${data.name}</>`;
+        })
+       document.body.innerHTML=output
+    }, 1000);
+}
+
+
+function createdata(newdata){
+    return new  Promise((resolve,reject) =>{
+        setTimeout(() =>{
+            datas.push(newdata);
+            let error=false;
+            if(!error){
+                resolve();
+            }else{
+                reject("kuch sai nahi ha")
+            }
+        }, 2000);
+    })
+   
+}
+// createdata({name:"Mostafa", Profestion:"software engineer"}).then(getDatas)
+ async function start(){
+    await createdata({name:"Mostafa", Profestion:"software engineer"})
+    getDatas();
+ }
+
+ start();
